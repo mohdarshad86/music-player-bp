87dc2b462afd14ff16072c03169bdc09133b06cd
Mohd Arshad <mohdarshad8668@gmail.com>
2023-05-09 19:14:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactAudioPlayer from 'react-audio-player';
+import { songs } from './songs';
+import './App.css'
 
 function App() {
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {songs.map((song, index) => (
+          <li key={index}>
+            <img src={song.cover} alt="Album cover" />
+            <div>
+              <h3>{song.title}</h3>
+              <p>{song.artist}</p>
+              <p>{song.album}</p>
+            </div>
+            <button onClick={() => setCurrentSong(song)}>Play</button>
+          </li>
+        ))}
+      </ul>
+      <div className='player'>
+        <ReactAudioPlayer src={currentSong.url} autoPlay />
+        <h2>{currentSong.title}</h2>
+        <p>{currentSong.artist}</p>
+        <p>{currentSong.album}</p>
+        <button onClick={() => setCurrentSong(songs[(songs.indexOf(currentSong) + songs.length - 1) % songs.length])}>Previous</button>
+        <button onClick={() => setCurrentSong(songs[(songs.indexOf(currentSong) + 1) % songs.length])}>Next</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/songs.js
@@ -0,0 +1,23 @@
+export const songs = [
+  {
+    title: 'Song Title 1',
+    artist: 'Artist Name 1',
+    album: 'Album Name 1',
+    url: 'https://example.com/song1.mp3',
+    cover: 'https://image.winudf.com/v2/image/Y29tLmZpbmFsYXBwLnBrc29uZ3Muc29uZ3Nwa19zY3JlZW5zaG90c18wX2NmOWE2NDVk/screen-0.jpg?fakeurl=1&type=.jpg'
+  },
+  {
+    title: 'Song Title 2',
+    artist: 'Artist Name 2',
+    album: 'Album Name 2',
+    url: 'https://example.com/song2.mp3',
+    cover: 'https://example.com/cover2.jpg'
+  },
+  {
+    title: 'Song Title 3',
+    artist: 'Artist Name 3',
+    album: 'Album Name 3',
+    url: 'https://example.com/song3.mp3',
+    cover: 'https://example.com/cover3.jpg'
+  },
+];
